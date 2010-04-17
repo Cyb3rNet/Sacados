6b069e5e78b536c5f6c0d66db3df069c4c53b81e
Serafim Junior Dos Santos <serafim@cyb3r.ca>
2010-04-17 16:19:03 -0400

    corrected test files

--- test/test.bp.api.list-items.inc.php
+++ test/test.bp.api.list-items.inc.php
@@ -22,32 +22,6 @@ $iListId = "";
 
 $oTCH->RegisterMethodWithReturn("List", array($iPageId, $iListId));
 
-//$sState = CGithubIssueStates::sOpen;
-//$oTCH->RegisterMethodWithReturn("ListIssues", array($sUser, $sRepo, $sState));
-
-//$iNumber = 1;
-//$oTCH->RegisterMethodWithReturn("ViewIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 5;
-//$oTCH->RegisterMethodWithReturn("ListCommentsByIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 8;
-//$oTCH->RegisterMethodWithReturn("CloseIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 4;
-//$oTCH->RegisterMethodWithReturn("ReOpenIssue", array($sUser, $sRepo, $iNumber));
-
-
-//$oTCH->RegisterMethodWithReturn("ListLabels", array($sUser, $sRepo));
-
-//$sLabel = "Label2";
-//$iNumber = "2";
-//$oTCH->RegisterMethodWithReturn("AddLabel", array($sUser, $sRepo, $sLabel, $iNumber));
-
-//$sLabel = "Label1";
-//$iNumber = "1";
-//$oTCH->RegisterMethodWithReturn("RemoveLabel", array($sUser, $sRepo, $sLabel, $iNumber));
-
 
 $oTCH->RunTestMap();
 
--- test/test.bp.api.pages.inc.php
+++ test/test.bp.api.pages.inc.php
@@ -20,32 +20,6 @@ $oTCH = new CTestClassHelper("CBackpackAPIPages", array($sAccountName, $sToken, 
 
 $oTCH->RegisterMethodWithReturn("ListAll", array());
 
-//$sState = CGithubIssueStates::sOpen;
-//$oTCH->RegisterMethodWithReturn("ListIssues", array($sUser, $sRepo, $sState));
-
-//$iNumber = 1;
-//$oTCH->RegisterMethodWithReturn("ViewIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 5;
-//$oTCH->RegisterMethodWithReturn("ListCommentsByIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 8;
-//$oTCH->RegisterMethodWithReturn("CloseIssue", array($sUser, $sRepo, $iNumber));
-
-//$iNumber = 4;
-//$oTCH->RegisterMethodWithReturn("ReOpenIssue", array($sUser, $sRepo, $iNumber));
-
-
-//$oTCH->RegisterMethodWithReturn("ListLabels", array($sUser, $sRepo));
-
-//$sLabel = "Label2";
-//$iNumber = "2";
-//$oTCH->RegisterMethodWithReturn("AddLabel", array($sUser, $sRepo, $sLabel, $iNumber));
-
-//$sLabel = "Label1";
-//$iNumber = "1";
-//$oTCH->RegisterMethodWithReturn("RemoveLabel", array($sUser, $sRepo, $sLabel, $iNumber));
-
 
 $oTCH->RunTestMap();
 
